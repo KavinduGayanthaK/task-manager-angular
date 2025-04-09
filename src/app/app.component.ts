99db883a8afe5280a56489a821c787e7ad90a0fa
Kavindu Gayantha <117292977+KavinduGayanthaK@users.noreply.github.com>
2025-04-09 22:23:46 +0530

    added material ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SideNavigationComponent } from "./side-navigation/side-navigation.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [SideNavigationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
